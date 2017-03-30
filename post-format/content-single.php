79594fae77b115cbc13d7dbb39769314c3717ef2
Andres Castillo <aazcast@gmail.com>
2017-03-30 14:33:02 -0600

    single post

--- post-format/content-single.php
+++ post-format/content-single.php
@@ -4,19 +4,20 @@
   $image = wp_get_attachment_image_src( get_post_thumbnail_id(the_ID()), 'single-post-thumbnail' );
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class($grv_post_class); ?> itemscope itemtype="https://schema.org/BlogPosting">
-  <div class="headerOtherPage">
     <?php if ( is_single()) : ?>
+        <div class="headerOtherPage singlePostBone cf" style="background: #000 url('<?php echo $image[0]; ?>') no-repeat;">
       <?php the_title('<h1 class="post-title">', '</h1>' ); ?>
+        </div>
     <?php else: ?>
+      <div class="headerOtherPage">
       <?php the_title( sprintf( '<h1 class="post-title"><a href="%s" itemprop="url" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+      </div><!-- /.post-head -->
     <?php endif; ?>
-  </div><!-- /.post-head -->
   <section class="container">
   <?php
     do_action( 'grv_post_meta' );
     do_action( 'grv_post_body' );
   ?>
-    <img src="<?php echo $image[0]; ?>">
     </section>
   <?php  if( is_single() ) : ?>
     <div class="post-foot">
